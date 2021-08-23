5cd330054ae09b240c17bf62e3aa12ef114efb9d
jeongmingi <mo4811@naver.com>
2021-08-23 22:57:42 +0900

    [Playground 학습테스트 실습]

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,23 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        String s = "1,2";
+        String[] dividedStr = s.split(",");
+        assertThat(dividedStr).contains("1");
+
+        s = "1";
+        dividedStr = s.split(",");
+        assertThat(dividedStr).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String s = "(1,2)";
+        String dividedStr = s.substring(1,4);
+        assertThat(dividedStr).isEqualTo("1,2");
+
+    }
 }
