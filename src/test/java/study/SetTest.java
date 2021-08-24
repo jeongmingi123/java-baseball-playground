e830e10bb731f2de816197ee0a77615077ed2c89
jeongmingi <mo4811@naver.com>
2021-08-24 23:29:06 +0900

    [Playground 단위 테스트 진행 중]

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,37 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void getSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void isNumbers(int number) {
+        assertThat(number > 0 && number < 4);
+    }
+
+}
